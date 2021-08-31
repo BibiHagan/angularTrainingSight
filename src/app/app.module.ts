9d3adb3dfe781cc86ef445c3e56af9dcb18f8501
Bibi <bibi.hagan@mondo.com.au>
2021-08-31 15:11:11 +1000

    product service complete product-details component start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from 'Angular-GettingStarted/APM-Final/src/app/shared/convert-to-spaces.pipe';
 import { StarComponent } from 'Angular-GettingStarted/APM-Final/src/app/shared/star.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductDetailComponent } from './products/product-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent, 
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   bootstrap: [AppComponent],
 })
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { ProductService } from "./product.service";
 import { IProduct } from "./product";
+import { Subscription } from "rxjs";
 
 @Component({
     selector: 'pm-products',
@@ -9,12 +11,14 @@ import { IProduct } from "./product";
     ]
 })
 
-export class ProductListComponent implements OnInit{
+export class ProductListComponent implements OnInit, OnDestroy{
     pageTitle = 'Product List';
     imageWidth = 50;
     imageMargin = 2;
     showImage: boolean = false;
-
+    errorMessage: string = '';
+    sub!: Subscription;
+    
     private _listFilter: string = '';
     get listFilter(): string{
         return this._listFilter;
@@ -22,40 +26,30 @@ export class ProductListComponent implements OnInit{
     set listFilter(value: string) {
         this._listFilter = value;
         this.filteredProducts = this.performFilter(value);
-        console.log(`In setter: ${value}`)
     }
 
     filteredProducts: IProduct[] = []
 
-    products: IProduct[] = [
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2021",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "assets/images/leaf_rake.png"
-        },
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2021",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-        },
-    ];
+    products: IProduct[] = [];
+
+    constructor( private productService: ProductService) {}
 
     toggleImage(): void {
         this.showImage = !this.showImage;
     }
 
     ngOnInit(): void {
-        this.listFilter = 'cart';
+        this.sub = this.productService.getProducts().subscribe({
+            next: products => {
+                this.products = products,
+                this.filteredProducts = this.products
+            },
+            error: err => this.errorMessage = err
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.sub.unsubscribe();
     }
 
     performFilter(filterBy: string): IProduct[] {
